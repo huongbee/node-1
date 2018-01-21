a6cbbe7013fb6912d2939959d07b00b1e7d0d5a8
Huong <you@example.com>
2018-01-22 00:15:58 +0700

    edit_sth

--- routes/book.js
+++ routes/book.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
-const Book = require('../models/book')
+const Book = require('../models/Book')
 const upload = require('../helpers/UploadFile');
 const multer = require("multer")()
 
