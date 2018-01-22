358a79ac54fd6d69852fbef7fa6e78e2746f2ace
Huong <you@example.com>
2018-01-23 00:21:39 +0700

    register-bcrypt

--- routes/book.js
+++ routes/book.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
-const Book = require('../models/Book')
+const Book = require('../models/Book.js')
 const upload = require('../helpers/UploadFile');
 const multer = require("multer")()
 
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const User = require('../models/User.js');
 const bodyParser = require('body-parser').urlencoded({ extended: false })
 
 
